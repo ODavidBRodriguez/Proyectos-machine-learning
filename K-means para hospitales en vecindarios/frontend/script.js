e076b31874734437680a5bf08a3da5ab171fd739
ODavidBRodriguez <167390460+ODavidBRodriguez@users.noreply.github.com>
2025-11-11 11:55:09 -0500

    Revert "Reapply "Cambio url agregar /api/locate""
    This reverts commit 36ed2123d33291eb356325b7bb90d59f1e3d5fb1.

--- K-means para hospitales en vecindarios/frontend/script.js
+++ K-means para hospitales en vecindarios/frontend/script.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'https://ubicador-hospitales.onrender.com/api/locate'; 
+const BACKEND_URL = 'https://ubicador-hospitales.onrender.com'; 
 
         // Paleta de colores para los clusters
         const COLORS = [
